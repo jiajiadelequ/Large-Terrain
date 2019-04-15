1d33353e4f8e2e0cb8ceaf9e78569d117563b137
jiajiadelequ <447263763@qq.com>
2019-04-16 00:12:46 +0800

    第二次提交 剔除了一些没用的文件

---
+++ Assets/BigMapLoad/Scripts/Core/MonoSingleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+
+            instance = FindObjectOfType(typeof(T)) as T;
+            if (instance != null)
+                return instance;
+
+            instance = new GameObject("_" + typeof(T).Name).AddComponent<T>();
+            return instance;
+        }
+    }
+}
---
+++ Assets/BigMapLoad/Scripts/Core/MonoThread.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoThread : MonoSingleton<MonoThread>
+{
+    public void Excute(IEnumerator en)
+    {
+        StartCoroutine(en);
+    }
+
+}
